c22f9a190091968eafc7c99df5c8d33dee4940a8
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-09-21 16:24:20 -0300

    create floor vertex

---
+++ shaders/floor/vertex.glsl
@@ -0,0 +1,10 @@
+attribute vec4 a_pos;
+attribute vec2 a_texCoord;
+uniform vec2 u_pos
+varying vec2 v_texCoord;
+void main()
+{
+  v_texCoord=a_texCoord;
+  vec2 pos=a_pos-u_pos;
+  gl_Position=vec4(pos,0,1);
+}
