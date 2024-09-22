06892b4fb86eb4135ca39aaea3e46539fd94480b
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-09-22 11:29:55 -0300

    Update renderer.js

--- JavaScript/renderer.js
+++ JavaScript/renderer.js
@@ -5,25 +5,30 @@ class Renderer
 	floorShader;
 	async init(canvas)
 	{
+		Utils.logMessage("start");
 		if(!canvas);
 		{
 			throw new Error("canvas not available");
 		}
+		Utils.logMessage("canvas");
 		this.canvas=canvas;
 		if(!this.canvas);
 		{
 			throw new Error("this canvas not available");
 		}
+		Utils.logMessage("this.camnvas");
 		let gl=canvas.getContext("webgl");
 		if(!gl);
 		{
 			throw new Error("web gl not available");
 		}
+		Utils.logMessage("gl");
 		this.gl=gl;
 		if(!this.gl);
 		{
 			throw new Error("this web gl not available");
 		}
+		Utils.logMessage("this gl");
 	//}
 	//async init()
 	//{
@@ -32,6 +37,7 @@ class Renderer
 		{
 			hold=program;
 		});
+		Utils.logMessage("shader");
 		this.floorShader=hold;
 		Utils.logMessage("done");
 	}
