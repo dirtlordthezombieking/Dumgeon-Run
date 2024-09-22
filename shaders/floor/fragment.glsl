61110603ec5748a47322695dccba2777f9975142
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-09-22 17:02:43 -0300

    Update fragment.glsl

--- shaders/floor/fragment.glsl
+++ shaders/floor/fragment.glsl
@@ -11,6 +11,6 @@ void main()
 	float cy=mod(8.99*cos(by),1.0);
 	float dx=floor(4.0*cx);
 	float dy=floor(4.0*cy);
-	vec2 texCoord=vec2(dx+mod(v_texCoord.x,1.0),dy+mod(v_texCoord.y,1.0))/4.0;
+	vec2 texCoord=floor(vec2(dx+mod(v_texCoord.x,1.0),dy+mod(v_texCoord.y,1.0))*32.0)/128.0;
 	gl_FragColor=texture2D(u_texture,texCoord);
 }
