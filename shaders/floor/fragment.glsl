3b8c8fd66ecf885adba32a4506c1220a1cd5aa80
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-09-22 17:08:16 -0300

    Update fragment.glsl

--- shaders/floor/fragment.glsl
+++ shaders/floor/fragment.glsl
@@ -5,10 +5,10 @@ void main()
 {
 	float ax=floor(v_texCoord.x);
 	float ay=floor(v_texCoord.y);
-	float bx=ax+0.129;
+	float bx=ax+0.371;
 	float by=ay+0.371;
 	float cx=mod(9.25*sin(bx),1.0);
-	float cy=mod(8.75*cos(by),1.0);
+	float cy=mod(9.25*sin(by),1.0);
 	float dx=floor(4.0*cx);
 	float dy=floor(4.0*cy);
 	vec2 texCoord=floor(vec2(dx+mod(v_texCoord.x,1.0),dy+mod(v_texCoord.y,1.0))*32.0)/128.0;
