6087553d8f19643892c73ddab5217e9a64a5f4b4
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-09-22 12:55:03 -0300

    Update renderer.js

--- JavaScript/renderer.js
+++ JavaScript/renderer.js
@@ -1,49 +1,32 @@
 class Renderer
 {
-	//gl;
-	//canvas;
-	//floorShader;
-	static init(g)
+	gl;
+	canvas;
+	floorShader;
+	static init(canvas)
+	{
+		this.canvas=canvas;
+		if(!this.canvas)
+		{
+			throw new Error("canvas invalid);
+		}
+		this.gl=this.canvas.getContext("webgl");;
+		if(!this.gl)
+		{
+			throw new Error("web gl not available");
+		}
+	}
+	async init()
 	{
 		try
 		{
-			Utils.logMessage("start");
-			//let c=document.querySelector("#canvas");
-			//if(!c);
-			//{
-				//throw new Error("canvas not available");
-			//}
-			//Utils.logMessage("canvas");
-			//this.canvas=c;
-			//if(!this.canvas);
-			//{
-				//throw new Error("this canvas not available");
-			//}
-			//Utils.logMessage("this.camnvas");
-			//let g=canvas.getContext("webgl");
-			if(!g);
-			{
-				throw new Error("web gl not available");
-			}
-			Utils.logMessage("gl");
-			//this.gl=g;
-			//if(!this.gl);
-			//{
-				//throw new Error("this web gl not available");
-			//}
-			Utils.logMessage("this gl");
-			throw new Error(g);
-	//}
-	//async init()
-	//{
 			let hold=0;
-			//await Utils.loadProgram(gl,"floor",function(program)
-			//{
-				//hold=program;
-			//});
-			//Utils.logMessage("shader");
-			//this.floorShader=hold;
-			//Utils.logMessage("done");
+			await Utils.loadProgram(gl,"floor",function(program)
+			{
+				hold=program;
+			});
+			this.floorShader=hold;
+			Utils.logMessage("done");
 		}
 		catch(e)
 		{
