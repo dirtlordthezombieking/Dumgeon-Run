707eedbd4dc5ddc6662ee02195e7f57c7f7a13a6
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-09-22 14:36:59 -0300

    Update renderer.js

--- JavaScript/renderer.js
+++ JavaScript/renderer.js
@@ -86,8 +86,8 @@ class Renderer
 	}
 	draw()
 	{
-		this.gl.clearColor(0,0,0,a);
-		this.gl.clear(this gl.COLOR_BUFFER_BIT);
+		this.gl.clearColor(0,0,0,1);
+		this.gl.clear(this.gl.COLOR_BUFFER_BIT);
 		this.gl.useProgram(this.floorShader);
 		this.floorATexCoord.use();
 		this.floorUTexture.use();
