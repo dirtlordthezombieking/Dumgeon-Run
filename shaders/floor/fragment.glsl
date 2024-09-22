f9ba8dc96ed7f77152255d8f777a3de44dab896a
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-09-22 19:45:44 -0300

    Update fragment.glsl

--- shaders/floor/fragment.glsl
+++ shaders/floor/fragment.glsl
@@ -9,8 +9,8 @@ void main()
 	float by=ay+0.371;
 	float cx=mod(9.25*sin(bx),1.0);
 	float cy=mod(8.99*cos(by),1.0);
-	float dx=mod(floor((4.0*cx)+ax),4.0);
-	float dy=mod(floor((4.0*cy)+ay),4.0);
+	float dx=mod(floor((4.0*cx)+ay),4.0);
+	float dy=mod(floor((4.0*cy)+ax),4.0);
 	vec2 texCoord=floor(vec2(dx+mod(v_texCoord.x,1.0),dy+mod(v_texCoord.y,1.0))*32.0)/128.0;
 	gl_FragColor=texture2D(u_texture,texCoord);
 }
