85094c5755df6dc49138becb3060be09bd9b73bb
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-09-22 15:56:48 -0300

    Update JavaScript

---
+++ JavaScript/attribute.js
@@ -0,0 +1,24 @@
+class Attribute
+{
+	size;
+	prog;
+	loc;
+	value;
+	gl;
+	constructor(attributeSize,shaderProgram,location,attributeValue,glCore)
+	{
+		this.size=attributeSize;
+		this.prog=shaderProgram;
+		this.loc=glCore.getAttribLocation(shaderProgram,location);
+		this.value=attributeValue;
+		this.gl=glCore;
+	}
+	use()
+	{
+		let buff=this.gl.createBuffer();
+		this.gl.bindBuffer(this.gl.ARRAY_BUFFER,buff);
+		this.gl.bufferData(this.gl.ARRAY_BUFFER,this.value,this.gl.STATIC_DRAW);
+		this.gl.enableVertexAttribArray(this.loc);
+		this.gl.vertexAttribPointer(this.loc,this.size,this.gl.FLOAT,false,0,0);
+	}
+}
