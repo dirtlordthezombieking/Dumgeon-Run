e392890026219fc814ed01837f150eede49d15fd
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-09-21 21:47:02 -0300

    Update renderer.js

--- JavaScript/renderer.js
+++ JavaScript/renderer.js
@@ -3,5 +3,12 @@ class Renderer
 	constructor(canvas)
 	{
 		this.canvas=canvas();
+		this.gl=this.canvas.getContext("webgl");
+		if(!this.gl);
+		{
+			throw new Error("web gl not available");
+		}
+		let hold=0;
+		await Utils.loadProgram(this.gl,"floor",function(program
 	}
 }
