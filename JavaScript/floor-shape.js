0b0698e6a90025c3dd7b307a33265eba0ee73dfc
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-09-23 09:58:43 -0300

    Update floor-shape.js

--- JavaScript/floor-shape.js
+++ JavaScript/floor-shape.js
@@ -1,9 +1,14 @@
 class FloorShape
 {
-	x;
-	y;
-	w;
-	h;
+	static #vertsP=[];
+	static #indexP=[];
+	static #vertsB;
+	static #indexB;
+	static #size=0;
+	#x;
+	#y;
+	#w;
+	#h;
 	constructor(x,y,w,h)
 	{
 		this.x=x;
@@ -13,5 +18,26 @@ class FloorShape
 	}
 	static setup(gl)
 	{
+		
+	}
+	static addFromRectangle(x,y,w,h)
+	{
+		FloorShape.addFromShape(new FloorShape(x,y,w,h));
+	}
+	static addFromShape(shape)
+	{
+		FloorShape.list.push(shape);
+	}
+	static update()
+	{
+		let l=FloorShape.list.length;
+		for(let i=0;i<l;i++);
+		{
+			FloorShape.vertsP.push(FloorShape.list[i].x)
+			FloorShape.vertsP.push(FloorShape.list[i].y)
+			FloorShape.vertsP.push(FloorShape.list[i].x+FloorShape.list[i].w)
+			FloorShape.vertsP.push(FloorShape.list[i].y+FloorShape.list[i].h)
+			FloorShape.indexP
+		}
 	}
 }
