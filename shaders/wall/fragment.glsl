d27f6f36b103af319dc4866c2be453eaf0bf760e
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-09-23 16:23:38 -0300

    Update fragment.glsl

--- shaders/wall/fragment.glsl
+++ shaders/wall/fragment.glsl
@@ -12,25 +12,19 @@ void main()
 	float dx=mod(floor(((4.0*cx)*ay)+ax),4.0);
 	float dy=mod(floor(((4.0*cy)*ax)+ay),4.0);
 	vec2 texCoord=floor(vec2(dx+mod(v_texCoord.x,1.0),dy+mod(v_texCoord.y,1.0))*32.0)/128.0;
-
-
 	float ex=floor(v_texCoord.x*32.0)/32.0;
 	float fx=sin(1.3*ex);
 	float gx=cos(0.8*ex);
 	float hx=(0.25*fx*gx)+0.75;
-
 	float ey=floor(v_texCoord.y*32.0)/32.0;
 	float fy=sin(1.2*ex);
 	float gy=cos(0.9*ex);
 	float hy=(0.25*fy*gy)+0.75;
-
 	float c=(hx+hy)/2.0;
-
 	float at=distance(vec2(ex,ey),vec2(0.0,0.0));
 	float bt=sin(1.4*at);
 	float ct=cos(0.7*at);
 	float dt=(0.25*bt*ct)+0.75;
-
 	float grime=((13.0*c)+(7.0*dt))/20.0;
 	gl_FragColor=texture2D(u_texture,texCoord)*grime;
 }
