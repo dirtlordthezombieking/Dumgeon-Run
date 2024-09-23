3383182e1077b2fbe09614d2fbc7d0f341196bf6
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-09-23 10:22:16 -0300

    Update floor-shape.js

--- JavaScript/floor-shape.js
+++ JavaScript/floor-shape.js
@@ -1,10 +1,14 @@
 class FloorShape
 {
+	static #list=[];
 	static #vertsP=[];
 	static #indexP=[];
+	static #vertsL;
+	static #indexL;
 	static #vertsB;
 	static #indexB;
 	static #size=0;
+	static #gl;
 	#x;
 	#y;
 	#w;
@@ -18,7 +22,7 @@ class FloorShape
 	}
 	static setup(gl)
 	{
-		
+		FloorShape.gl=gl;
 	}
 	static addFromRectangle(x,y,w,h)
 	{
@@ -28,16 +32,41 @@ class FloorShape
 	{
 		FloorShape.list.push(shape);
 	}
+	static reset()
+	{
+		FloorShape.list=[];
+	}
 	static update()
 	{
 		let l=FloorShape.list.length;
+		FloorShape.vertsP=[];
+		FloorShape.indexP=[];
 		for(let i=0;i<l;i++);
 		{
-			FloorShape.vertsP.push(FloorShape.list[i].x)
-			FloorShape.vertsP.push(FloorShape.list[i].y)
-			FloorShape.vertsP.push(FloorShape.list[i].x+FloorShape.list[i].w)
-			FloorShape.vertsP.push(FloorShape.list[i].y+FloorShape.list[i].h)
-			FloorShape.indexP
+			FloorShape.vertsP.push(FloorShape.list[i].x);
+			FloorShape.vertsP.push(FloorShape.list[i].y);
+			FloorShape.vertsP.push(FloorShape.list[i].x+FloorShape.list[i].w);
+			FloorShape.vertsP.push(FloorShape.list[i].y);
+			FloorShape.vertsP.push(FloorShape.list[i].x);
+			FloorShape.vertsP.push(FloorShape.list[i].y+FloorShape.list[i].h);
+			FloorShape.vertsP.push(FloorShape.list[i].x+FloorShape.list[i].w);
+			FloorShape.vertsP.push(FloorShape.list[i].y+FloorShape.list[i].h);
+			let p=i*4;
+			FloorShape.indexP.push(p);
+			FloorShape.indexP.push(p+1);
+			FloorShape.indexP.push(p+2);
+			FloorShape.indexP.push(p+2);
+			FloorShape.indexP.push(p+1);
+			FloorShape.indexP.push(p+3);
 		}
 	}
+	static prep()
+	{
+		FloorShape.vertsB=FloorShape.gl.createBuffer();
+			FloorShape.gl.bindBuffer(FloorShape.gl.ELEMENT_ARRAY_BUFFER,indexB);
+		FloorShape.gl.bufferData(FloorShape.gl.ELEMENT_ARRAY_BUFFER,new Uint16Array(indexA),FloorShape.gl.STATIC_DRAW
+);
+	}
+	static Draw()
 }
+//FloorShape.gl.
