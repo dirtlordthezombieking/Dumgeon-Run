65b485cb1b8e281ed0d5a2d080008bfd6ecc7050
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-09-23 15:47:43 -0300

    Update shaders

---
+++ shaders/wall/vertex.glsl
@@ -0,0 +1,9 @@
+attribute vec2 a_pos;
+uniform vec2 u_pos;
+varying vec2 v_texCoord;
+void main()
+{
+	v_texCoord=a_pos/64.0;
+	vec2 pos=(a_pos-u_pos)/vec2(320.0,240.0);
+	gl_Position=vec4(pos,0,1);
+}
