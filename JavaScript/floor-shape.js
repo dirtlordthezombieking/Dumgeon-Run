1555baa31c485f186d4d83af5644bdae6af024c2
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-09-23 13:54:17 -0300

    Update floor-shape.js

--- JavaScript/floor-shape.js
+++ JavaScript/floor-shape.js
@@ -17,10 +17,10 @@ class FloorShape
 	#h;
 	constructor(x,y,w,h)
 	{
-		this.x=x;
-		this.y=y;
-		this.w=w;
-		this.h=h;
+		this.#x=x;
+		this.#y=y;
+		this.#w=w;
+		this.#h=h;
 	}
 	static setup(gl,onDone)
 	{
@@ -31,12 +31,12 @@ Utils.logMessage("start");
 			let hold=0
 			Utils.loadShader(gl,"floor",function(program)
 			{
-				FloorShape.shader=program;
+				FloorShape.#shader=program;
 Utils.logMessage("shader");
 				Utils.loadImage("graphics/tilesets/floors.png",function(img)
 				{
 Utils.logMessage("run");
-					FloorShape.tex=img;
+					FloorShape.#tex=img;
 					onDone();
 Utils.logMessage("end");
 				});
@@ -53,56 +53,56 @@ Utils.logMessage("end");
 	}
 	static addFromShape(shape)
 	{
-		FloorShape.list.push(shape);
+		FloorShape.#list.push(shape);
 	}
 	static reset()
 	{
-		FloorShape.list=[];
+		FloorShape.#list=[];
 	}
 	static update()
 	{
-		let l=FloorShape.list.length;
-		FloorShape.vertsP=[];
-		FloorShape.indexP=[];
-		FloorShape.size=l*6;
+		let l=FloorShape.#list.length;
+		FloorShape.#vertsP=[];
+		FloorShape.#indexP=[];
+		FloorShape.#size=l*6;
 		for(let i=0;i<l;i++);
 		{
-			FloorShape.vertsP.push(FloorShape.list[i].x);
-			FloorShape.vertsP.push(FloorShape.list[i].y);
-			FloorShape.vertsP.push(FloorShape.list[i].x+FloorShape.list[i].w);
-			FloorShape.vertsP.push(FloorShape.list[i].y);
-			FloorShape.vertsP.push(FloorShape.list[i].x);
-			FloorShape.vertsP.push(FloorShape.list[i].y+FloorShape.list[i].h);
-			FloorShape.vertsP.push(FloorShape.list[i].x+FloorShape.list[i].w);
-			FloorShape.vertsP.push(FloorShape.list[i].y+FloorShape.list[i].h);
+			FloorShape.#vertsP.push(FloorShape.list[i].x);
+			FloorShape.#vertsP.push(FloorShape.list[i].y);
+			FloorShape.#vertsP.push(FloorShape.list[i].x+FloorShape.list[i].w);
+			FloorShape.#vertsP.push(FloorShape.list[i].y);
+			FloorShape.#vertsP.push(FloorShape.list[i].x);
+			FloorShape.#vertsP.push(FloorShape.list[i].y+FloorShape.list[i].h);
+			FloorShape.#vertsP.push(FloorShape.list[i].x+FloorShape.list[i].w);
+			FloorShape.#vertsP.push(FloorShape.list[i].y+FloorShape.list[i].h);
 			let p=i*4;
-			FloorShape.indexP.push(p);
-			FloorShape.indexP.push(p+1);
-			FloorShape.indexP.push(p+2);
-			FloorShape.indexP.push(p+2);
-			FloorShape.indexP.push(p+1);
-			FloorShape.indexP.push(p+3);
+			FloorShape.#indexP.push(p);
+			FloorShape.#indexP.push(p+1);
+			FloorShape.#indexP.push(p+2);
+			FloorShape.#indexP.push(p+2);
+			FloorShape.#indexP.push(p+1);
+			FloorShape.#indexP.push(p+3);
 		}
 	}
 	static prep()
 	{
-		FloorShape.vertsB=FloorShape.gl.createBuffer();
-			FloorShape.gl.bindBuffer(FloorShape.gl.ELEMENT_ARRAY_BUFFER,indexB);
-		FloorShape.gl.bufferData(FloorShape.gl.ELEMENT_ARRAY_BUFFER,new Uint16Array(indexA),FloorShape.gl.STATIC_DRAW
+		FloorShape.#vertsB=FloorShape.#gl.createBuffer();
+			FloorShape.#gl.bindBuffer(FloorShape.#gl.ELEMENT_ARRAY_BUFFER,FloorShape.#indexB);
+		FloorShape.#gl.bufferData(FloorShape.#gl.ELEMENT_ARRAY_BUFFER,new Uint16Array(indexA),FloorShape.#gl.STATIC_DRAW
 );
-		FloorShape.uTexture=new Texture(FloorShape.shader,"u_texture",FloorShape.tex,0,FloorShape.gl);
-		FloorShape.uTexture.push();
-		FloorShape.aPos=new Attribute(2,FloorShape.shader,"a_pos",new Float32Array(FloorShape.vertsP),FloorShape.gl);
-		FloorShape.uPos=new Uniform(2,FloorShape.shader,"u_pos",[0.0,0.0],FloorShape.gl)
+		FloorShape.#uTexture=new Texture(FloorShape.#shader,"u_texture",FloorShape.#tex,0,FloorShape.#gl);
+		FloorShape.#uTexture.push();
+		FloorShape.#aPos=new Attribute(2,FloorShape.#shader,"a_pos",new Float32Array(FloorShape.#vertsP),FloorShape.#gl);
+		FloorShape.#uPos=new Uniform(2,FloorShape.#shader,"u_pos",[0.0,0.0],FloorShape.#gl)
 	}
 	static Draw()
 	{
-		FloorShape.gl.useProgram(FloorShape.shader);
-		FloorShape.uTexture.use();
-		FloorShape.aPos.use();
-		FloorShape.uPos.use();
-	FloorShape.gl.bindBuffer(FloorShape.gl.ELEMENT_ARRAY_BUFFER,indexB);	FloorShape.gl.drawElements(FloorShape.gl.TRIANGLES,size,FloorShape.gl.UNSIGNED_SHORT,0);
+		FloorShape.#gl.useProgram(FloorShape.#shader);
+		FloorShape.#uTexture.use();
+		FloorShape.#aPos.use();
+		FloorShape.#uPos.use();
+	FloorShape.#gl.bindBuffer(FloorShape.#gl.ELEMENT_ARRAY_BUFFER,FloorShape.#indexB);	FloorShape.#gl.drawElements(FloorShape.#gl.TRIANGLES,FloorShape.#size,FloorShape.#gl.UNSIGNED_SHORT,0);
 	}
 }
 //FloorShape.gl.
-//FloorShape.
+//FloorShape.#
