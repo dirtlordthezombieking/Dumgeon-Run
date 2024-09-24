4a4cb75d3d2ae284afc4180d8bdf397fcfc6a2f9
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-09-24 07:56:10 -0300

    Update floor.js

--- JavaScript/floor.js
+++ JavaScript/floor.js
@@ -51,7 +51,7 @@ class Floor
 		l=this.#connected.length;
 		for(let i=0;i<l;i++)
 		{
-			let r=floor.#rooms[floor.#connected[i]];
+			let r=this.#rooms[this.#connected[i]];
 			WallShape.addFromRectangle((r.x-50)*4,(r.y-50)*4,r.w*4,r.h*4);
 		}
 		//let r=this.#rooms[0];
