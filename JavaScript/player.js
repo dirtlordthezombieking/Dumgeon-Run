873973eb0527bbcf0bf11c6fe48f473b4b969d75
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-09-24 15:26:48 -0300

    Update player.js

--- JavaScript/player.js
+++ JavaScript/player.js
@@ -1,4 +1,22 @@
 class Player()
 {
-	init(gl
+	#shader;
+	#apos;
+	#u_eye;
+	static setup(gl)
+	{
+		try
+		{
+			OverhangShape.#gl=gl;
+			Utils.loadShader(gl,"player",function(program)
+			{
+				Player.#shader=program;
+				onDone();
+			});
+		}
+		catch(e)
+		{
+			Utils.logMessage("error:\n"+e.message);
+		}
+	}
 }
