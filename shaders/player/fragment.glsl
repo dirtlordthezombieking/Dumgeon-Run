47df0b302dcbad4221c7e6d91d5793abcd8b44ba
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-09-24 16:39:12 -0300

    Update fragment.glsl

--- shaders/player/fragment.glsl
+++ shaders/player/fragment.glsl
@@ -8,7 +8,7 @@ void main()
 	float rad1=distance(pix,vec2(0.0,0.0));
 	float clip1=pi*min(rad1,16.0);
 	float flat1=clip1/16.0;
-	float wave1=sin(flat1);
+	float wave1=cos(flat1);
 	float pre1=sign(wave1);
 	float bodymult=(pre1+1.0)/2.0;
 	vec4 body=vec4(0.05,0.2,0.8,1.0)*bodymult;
