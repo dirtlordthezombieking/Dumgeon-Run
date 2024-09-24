3e3472620b387a5392b06425c7ec341a735af4d4
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-09-24 15:27:37 -0300

    Update player.js

--- JavaScript/player.js
+++ JavaScript/player.js
@@ -19,4 +19,27 @@ class Player()
 			Utils.logMessage("error:\n"+e.message);
 		}
 	}
+	static prep()
+	{
+		FloorShape.#indexB=FloorShape.#gl.createBuffer();
+		FloorShape.#gl.bindBuffer(FloorShape.#gl.ELEMENT_ARRAY_BUFFER,FloorShape.#indexB);
+		FloorShape.#gl.bufferData(FloorShape.#gl.ELEMENT_ARRAY_BUFFER,new Uint16Array(FloorShape.#indexP),FloorShape.#gl.STATIC_DRAW);
+		FloorShape.#uTexture=new Texture(FloorShape.#shader,"u_texture",FloorShape.#tex,0,FloorShape.#gl);
+		FloorShape.#uTexture.push();
+		FloorShape.#aPos=new Attribute(2,FloorShape.#shader,"a_pos",new Float32Array(FloorShape.#vertsP),FloorShape.#gl);
+		FloorShape.#uPos=new Uniform(2,FloorShape.#shader,"u_pos",[0.0,0.0],FloorShape.#gl);
+	}
+	static draw()
+	{
+		if(FloorShape.#size===0)
+		{
+			return;
+		}
+		FloorShape.#gl.useProgram(FloorShape.#shader);
+		FloorShape.#uTexture.use();
+		FloorShape.#aPos.use();
+		FloorShape.#uPos.use();
+		FloorShape.#gl.bindBuffer(FloorShape.#gl.ELEMENT_ARRAY_BUFFER,FloorShape.#indexB);
+		FloorShape.#gl.drawElements(FloorShape.#gl.TRIANGLES,FloorShape.#size,FloorShape.#gl.UNSIGNED_SHORT,0);
+	}
 }
