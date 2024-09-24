b646ac7efbbe3aff4eb35e7f6211cf39ac43af0e
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-09-24 15:37:01 -0300

    Update player.

--- JavaScript/player.js
+++ JavaScript/player.js
@@ -23,8 +23,20 @@ class Player()
 	{
 		Player.#indexB=Player.#gl.createBuffer();
 		Player.#gl.bindBuffer(Player.#gl.ELEMENT_ARRAY_BUFFER,Player.#indexB);
-		Player.#gl.bufferData(Player.#gl.ELEMENT_ARRAY_BUFFER,new Uint16Array(Player.#indexP),Player.#gl.STATIC_DRAW);
-		Player.#aPos=new Attribute(2,Player.#shader,"a_pos",new Float32Array(Player.#vertsP),Player.#gl);
+		Player.#gl.bufferData(Player.#gl.ELEMENT_ARRAY_BUFFER,new Uint16Array(
+				[
+					0,1,2,
+					2,1,3
+				]
+			),Player.#gl.STATIC_DRAW);
+		Player.#aPos=new Attribute(2,Player.#shader,"a_pos",new Float32Array(
+			[
+				-32,-32,
+				 32,-32,
+				-32, 32,
+				 32, 32
+			]
+		),Player.#gl);
 		Player.#uEye=new Uniform(2,Player.#shader,"u_eye",[0.0,16.0,24.0],Player.#gl);
 	}
 	static draw()
@@ -40,3 +52,5 @@ class Player()
 		Player.#gl.drawElements(Player.#gl.TRIANGLES,Player.#size,Player.#gl.UNSIGNED_SHORT,0);
 	}
 }
+//ss,es,se,ee
+//0,1,2,2,1,3
