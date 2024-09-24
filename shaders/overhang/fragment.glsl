d4f88695a606a94ac10c8e6d29fa5d344449ec99
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-09-24 14:22:27 -0300

    Update fragment.glsl

--- shaders/overhang/fragment.glsl
+++ shaders/overhang/fragment.glsl
@@ -4,5 +4,5 @@ void main()
 {
 	vec2 pix=floor(v_texCoord/2.0);
 	float alpha=(2.0*(clamp(distance(pix,vec2(0.0,0.0))/64.0,0.5,1.0)))-1.0;
-	gl_FragColor=vec4(0.0,0.0,alpha,1.0);
+	gl_FragColor=vec4(0.0,alpha,0.0,alpha);
 }
