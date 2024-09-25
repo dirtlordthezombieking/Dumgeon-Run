ec7dc42a783aaf7f283ae6dd58834bf3f21d5e07
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-09-25 18:09:23 -0300

    Update vertex.glsl

--- shaders/back/vertex.glsl
+++ shaders/back/vertex.glsl
@@ -4,6 +4,6 @@ uniform vec3 u_pos;
 varying vec2 v_texCoord;
 void main()
 {
-	v_texCoord=(a_pos*vec2(320,240))-((u_pos.xy*0.25))*64.0);
+	v_texCoord=(a_pos*vec2(320,240))-((u_pos.xy*0.25)*64.0);
 	gl_Position=vec4(a_pos,0,1);
 }
