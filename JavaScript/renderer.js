4512f051fb6bd53da349d445b6b1a35cbe3a5357
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-09-25 12:06:11 -0300

    Update renderer.js

--- JavaScript/renderer.js
+++ JavaScript/renderer.js
@@ -57,21 +57,12 @@ class Renderer
 			this.canvas.height=displayHeight;
 		}
 		this.gl.enable(this.gl.CULL_FACE);
-		//this.gl.enable(this.gl.DEPTH_TEST);
 		this.gl.enable(this.gl.BLEND);
 		this.gl.blendFunc(this.gl.SRC_ALPHA,this.gl.ONE_MINUS_SRC_ALPHA);
 		this.gl.viewport(0,0,this.gl.canvas.width,this.gl.canvas.height);
-		//FloorShape.addFromRectangle(-640,-480,500,960);
-		//FloorShape.addFromRectangle( 140,-480,500,960);
-		//FloorShape.addFromRectangle(-140, 140,280,340);
-		//FloorShape.addFromRectangle(-140,-480,280,340);
 		this.floor.use();
 		FloorShape.update();
 		FloorShape.prep();
-		//WallShape.addFromRectangle(-140,-140, 30,280);
-		//WallShape.addFromRectangle( 110,-140, 30,280);
-		//WallShape.addFromRectangle(-110,-140,220, 30);
-		//WallShape.addFromRectangle(-110, 120,220, 30);
 		WallShape.update();
 		WallShape.prep();
 		Player.prep()
@@ -82,7 +73,7 @@ class Renderer
 		this.pos=this.floor.getPos();
 		Utils.logMessage("inpos: "+this.pos[0]+", "+this.pos[1]);
 		let tis=this;
-		requestAnimationFrame(function(ts){tis.draw(ts);});//,0);
+		requestAnimationFrame(function(ts){tis.draw(ts);});
 		this.canvas.requestFullscreen();
 	}
 	fullscreen()
@@ -91,20 +82,17 @@ class Renderer
 	}
 	draw(t)
 	{
-		//Utils.logMessage("t: "+t);
 		let d=t-this.frameTime;
 		this.frameTime=performance.now();
-		//Utils.logMessage("t: "+t+", time: "+this.frameTime);
 		Utils.logMessage("drawpos: "+this.pos[0]+", "+this.pos[1]);
 		try
 		{
 			this.gl.clearColor(0,0,0,1);
-			this.gl.clear(this.gl.COLOR_BUFFER_BIT);//|this.gl.DEPTH_BUFFER_BIT);
+			this.gl.clear(this.gl.COLOR_BUFFER_BIT);
 			FloorShape.draw(d,this.pos);
 			WallShape.draw(d,this.pos);
 			Player.draw(d);
 			OverhangShape.draw(d,this.pos);
-			//let time=Date.now()-this.frameTime;
 			this.errframes=0
 		}
 		catch(e)
@@ -122,18 +110,14 @@ class Renderer
 			}
 			else
 			{
-				//if(time>33)
-				//{
-					//time=33;
-				//}
 				let tis=this;
-				//requestAnimationFrame(function(ts){tis.draw(ts);});//,33-time);
+				//requestAnimationFrame(function(ts){tis.draw(ts);});
 			}
 		}
 		catch(e)
 		{
 			this.errframes++;
-			//Utils.logMessage("error:\n"+e.message);
+			Utils.logMessage("error:\n"+e.message);
 		}
 	}
 }
