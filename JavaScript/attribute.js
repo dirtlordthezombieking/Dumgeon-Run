6b3e8d5de41817f1cad1c72ce5cf9de842040a63
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-09-25 09:04:21 -0300

    Update attribute.js

--- JavaScript/attribute.js
+++ JavaScript/attribute.js
@@ -14,7 +14,7 @@ class Attribute
 		this.value=attributeValue;
 		this.gl=glCore;
 		this.buff=this.gl.createBuffer();
-		this.gl.bindBuffer(this.gl.ARRAY_BUFFER,buff);
+		this.gl.bindBuffer(this.gl.ARRAY_BUFFER,this.buff);
 		this.gl.bufferData(this.gl.ARRAY_BUFFER,this.value,this.gl.STATIC_DRAW);
 	}
 	use()
