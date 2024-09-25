38960ed1f3300b6277bd7fd4ab6adad492c7e0f9
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-09-25 10:08:47 -0300

    Update fragment.glsl

--- shaders/player/fragment.glsl
+++ shaders/player/fragment.glsl
@@ -11,7 +11,8 @@ void main()
 	float wave1=cos(flat1);
 	float pre1=sign(wave1);
 	float bodymult=floor((pre1+1.0)/2.0);
-	vec4 body=vec4(0.05,0.2,0.8,1.0)*bodymult;
+	float bodysmooth=(0.25.wave1*bodymult)+0.75;
+	vec4 body=vec4(0.05,0.2,0.8,1.0)*bodymult*bodysmooth;
 
 	vec2 theta=vec2(sin(u_eye.x),cos(u_eye.x));
 
@@ -20,7 +21,8 @@ void main()
 	float flat2=clip2/8.5;
 	float wave2=cos(flat2);
 	float pre2=sign(wave2);
-	float prepreeyemult=floor((pre2+1.0)/2.0);
+	float preeyemult=floor((pre2+1.0)/2.0);
+	float eyesmooth=(0.25.wave2*preeyemult)+0.75;
 
 	float rad3=distance(pix,theta*u_eye.z);
 	float clip3=pi*min(rad3,4.5);
@@ -28,11 +30,11 @@ void main()
 	float wave3=cos(flat3);
 	float pre3=sign(wave3);
 	float prepupmult=(pre3+1.0)/2.0;
-	float pupmult=floor((prepupmult+prepreeyemult)*0.55);
-	vec4 pup=vec4(0.05,0.8,0.2,1.0)*pupmult;
+	float pupmult=floor((prepupmult+preeyemult)*0.55);
+	vec4 pup=vec4(0.05,0.8,0.2,1.0)*pupmult*eyesmooth;
 
-	float preeyemult=floor((((((prepreeyemult-prepupmult)+1.0)/2.0)-bodymult)+1.0)/2.0);
-	vec4 eye=vec4(0.9,0.9,0.9,1.0)*preeyemult;
+	float eyemult=floor((((((preeyemult-prepupmult)+1.0)/2.0)-bodymult)+1.0)/2.0);
+	vec4 eye=vec4(0.9,0.9,0.9,1.0)*eyemult*eyesmooth;
 
 	gl_FragColor=body+pup+eye;
 }
