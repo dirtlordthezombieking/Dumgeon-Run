54558f842cf6e924f9d240bd405432666fdddeb3
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-09-25 16:57:22 -0300

    Update vertex.glsl

--- shaders/border/vertex.glsl
+++ shaders/border/vertex.glsl
@@ -2,6 +2,6 @@ attribute vec2 a_pos;
 uniform vec2 u_pos;
 void main()
 {
-	vec2 pos=(a_pos-u_pos)*64/vec2(320.0,240.0);
+	vec2 pos=(a_pos-u_pos)*64.0/vec2(320.0,240.0);
 	gl_Position=vec4(pos,0,1);
 }
