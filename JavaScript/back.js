62cfed8ba7697846895b6f11132294399aedbc4a
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-09-25 13:18:52 -0300

    Update back.js

--- JavaScript/back.js
+++ JavaScript/back.js
@@ -2,7 +2,7 @@ class Back
 {
 	static #shader;
 	static #aPos;
-	static #uEye;
+	static #uPos;
 	static #gl;
 	static #indexB;
 	static #angle=0;
@@ -11,7 +11,7 @@ class Back
 		try
 		{
 			Back.#gl=gl;
-			Utils.loadShader(gl,"player",function(program)
+			Utils.loadShader(gl,"back",function(program)
 			{
 				Back.#shader=program;
 				onDone();
@@ -24,35 +24,35 @@ class Back
 	}
 	static prep()
 	{
-		Player.#indexB=Player.#gl.createBuffer();
-		Player.#gl.bindBuffer(Player.#gl.ELEMENT_ARRAY_BUFFER,Player.#indexB);
-		Player.#gl.bufferData(Player.#gl.ELEMENT_ARRAY_BUFFER,new Uint16Array
+		Back.#indexB=Back.#gl.createBuffer();
+		Back.#gl.bindBuffer(Back.#gl.ELEMENT_ARRAY_BUFFER,Back.#indexB);
+		Back.#gl.bufferData(Back.#gl.ELEMENT_ARRAY_BUFFER,new Uint16Array
 		(
 			[
 				0,1,2,
 				2,1,3
 			]
-		),Player.#gl.STATIC_DRAW);
-		Player.#aPos=new Attribute(2,Player.#shader,"a_pos",new Float32Array
+		),Back.#gl.STATIC_DRAW);
+		Back.#aPos=new Attribute(2,Back.#shader,"a_pos",new Float32Array
 		(
 			[
-				-32,-32,
-				 32,-32,
-				-32, 32,
-				 32, 32
+				-2,-2,
+				 2,-2,
+				-2, 2,
+				 2, 2
 			]
-		),Player.#gl);
-		Player.#uEye=new Uniform(3,Player.#shader,"u_eye",[Math.PI,8.0,12.0],Player.#gl);
+		),Back.#gl);
+		Back.#uPos=new Uniform(2,Back.#shader,"u_pos",[0.0,0.0],Back.#gl);
 	}
-	static draw(t)
+	static draw(t,off)
 	{
-		Player.#uEye.set([Player.#angle*Math.PI/180,8.0,12.0])
-		Player.#gl.useProgram(Player.#shader);
-		Player.#aPos.use();
-		Player.#uEye.use();
-		Player.#angle+=t*0.03;
-		Player.#gl.bindBuffer(Player.#gl.ELEMENT_ARRAY_BUFFER,Player.#indexB);
-		Player.#gl.drawElements(Player.#gl.TRIANGLES,6,Player.#gl.UNSIGNED_SHORT,0);
+		Back.#gl.useProgram(Back.#shader);
+		Back.#uPos.set(off);
+		Back.#aPos.use();
+		Back.#uPos.use();
+		Back.#angle+=t*0.03;
+		Back.#gl.bindBuffer(Back.#gl.ELEMENT_ARRAY_BUFFER,Back.#indexB);
+		Back.#gl.drawElements(Back.#gl.TRIANGLES,6,Back.#gl.UNSIGNED_SHORT,0);
 	}
 }
 //ss,es,se,ee
