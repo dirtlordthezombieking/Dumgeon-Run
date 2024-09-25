19e6fa91f6e443aab998b34304e81cda07a0d0df
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-09-25 13:58:32 -0300

    Update fragment.glsl

--- shaders/back/fragment.glsl
+++ shaders/back/fragment.glsl
@@ -7,8 +7,8 @@ void main()
 	float val2=sqrt((pix.x*pix.x)+(pix.y*pix.y));
 	float xy=pix.x*pix.y;
 	float val3=sqrt((xy+xy)*sign(xy));
-	float red=((sin(val3)+cos(val1))*0.1)+0.1;
-	float green=((sin(val2)+cos(val3))*0.15)+0.15;
-	float blue=((sin(val1)+cos(val2))*0.05)+0.05;
+	float red=((sin(val3)+cos(val1))*0.05)+0.2;
+	float green=((sin(val2)+cos(val3))*0.05)+0.25;
+	float blue=((sin(val1)+cos(val2))*0.05)+0.15;
 	gl_FragColor=vec4(red,green,blue,1.0);
 }
