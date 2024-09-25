5dde78f352f85040871c9fd044afe92c80be8fc9
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-09-25 15:33:18 -0300

    Update fragment.glsl

--- shaders/back/fragment.glsl
+++ shaders/back/fragment.glsl
@@ -1,12 +1,13 @@
 precision mediump float;
+uniform vec3 u_pos;
 varying vec2 v_texCoord;
 void main()
 {
 	vec2 pix=floor(v_texCoord);
-	float val1=pix.x+pix.y;
-	float val2=sqrt((pix.x*pix.x)+(pix.y*pix.y));
+	float val1=pix.x+pix.y+u_pos.z;
+	float val2=sqrt((pix.x*pix.x)+(pix.y*pix.y))+u_pos.z;
 	float xy=pix.x*pix.y;
-	float val3=sqrt((xy+xy)*sign(xy));
+	float val3=sqrt((xy+xy)*sign(xy))+u_pos.z;
 	float red=((sin(val3)*cos(val1))*0.05)+0.15;
 	float green=((sin(val2)*cos(val3))*0.05)+0.2;
 	float blue=((sin(val1)*cos(val2))*0.05)+0.1;
