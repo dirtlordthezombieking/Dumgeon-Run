f2c522e87483a56b25f4ac416a8fb6e40ea82507
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-09-26 20:59:47 -0300

    Update vertex.glsl

--- shaders/minimap/vertex.glsl
+++ shaders/minimap/vertex.glsl
@@ -2,6 +2,6 @@ precision mediump float;
 attribute vec2 a_pos;
 void main()
 {
-	vec2 pos=(a_pos-vec2(50,50))/vec2(150,150);
-	gl_Position=vec4(pos,0,1);
+	vec2 pos=(a_pos-vec2(50.0,50.0))/vec2(150.0,150.0);
+	gl_Position=vec4(pos,0.0,1.0);
 }
