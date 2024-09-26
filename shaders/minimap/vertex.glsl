ee165adeba6513bec8152ed41293d1ad55cee7bf
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-09-26 14:15:54 -0300

    Update shaders

---
+++ shaders/minimap/vertex.glsl
@@ -0,0 +1,10 @@
+precision mediump float;
+attribute vec2 a_pos;
+uniform vec2 u_pos;
+varying vec2 v_texCoord;
+void main()
+{
+	v_texCoord=(a_pos*vec2(320,240))-((u_pos.xy*0.25)*64.0);
+	float pos=
+	gl_Position=vec4(a_pos,0,1);
+}
